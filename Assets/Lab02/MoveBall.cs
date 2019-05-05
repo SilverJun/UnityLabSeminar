a29fa3e0f09056e8dcd9ef2ba27af2d1484b4c35
SilverJun <jangej1031@naver.com>
2019-05-06 02:27:13 +0900

    Add lab02

---
+++ Assets/Lab02/MoveBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    private Rigidbody _rigid;
+    
+    [SerializeField] [Range(0.0f,5.0f)]
+    private float _force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _rigid.AddForce(transform.position.red, _force);
+        }
+        
+    }
+}
