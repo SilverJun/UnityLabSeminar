ff89955631dde28ebf3c13068e3deebb5980ecbb
SilverJun <jangej1031@naver.com>
2019-05-06 19:45:46 +0900

    Upload Lab04

---
+++ Assets/Lab04/PlayerPrefsLab.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsLab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("IsFirst"))
+        {
+            Debug.Log("처음 실행 아님!");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsFirst", 1);
+            PlayerPrefs.Save();
+            Debug.Log("처음 실행임!");
+        }
+    }
+}
