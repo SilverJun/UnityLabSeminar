d1b233ab43218bb664ccc259db7422d9a75dc88a
SilverJun <jangej1031@naver.com>
2019-05-06 18:18:51 +0900

    Upload Lab02

--- Assets/Lab02/MoveBall.cs
+++ Assets/Lab02/MoveBall.cs
@@ -6,7 +6,7 @@ public class MoveBall : MonoBehaviour
 {
     private Rigidbody _rigid;
     
-    [SerializeField] [Range(0.0f,5.0f)]
+    [SerializeField] [Range(0.0f,30.0f)]
     private float _force;
 
     // Start is called before the first frame update
@@ -18,10 +18,21 @@ public class MoveBall : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            _rigid.AddForce(transform.position.red, _force);
+            _rigid.AddForce(-Vector3.right * _force);
         }
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigid.AddForce(Vector3.right * _force);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _rigid.AddForce(Vector3.forward * _force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _rigid.AddForce(-Vector3.forward * _force);
+        }
     }
 }
